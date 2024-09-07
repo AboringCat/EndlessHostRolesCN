7750eb8323f0a803276be65993f3555017ae1c78
AboringCat <129568550+AboringCat@users.noreply.github.com>
2024-09-07 19:25:08 +0800

    Update and rename AnnouncementPatch.cs to ModNewsPatch.cs

--- Patches/ModNewsPatch.cs
+++ Patches/ModNewsPatch.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 using LibCpp2IL;
 
-namespace TOHE;
+namespace EHR;
 
 // code credit https://github.com/Yumenopai/TownOfHost_Y
 [HarmonyPatch]
@@ -18,39 +18,38 @@ public class ModNews
 {
     public int Number;
     public int BeforeNumber;
-    public string Title;
-    public string SubTitle;
-    public string ShortTitle;
-    public string Text;
-    public string Date;
+    public string NewsTitle;
+    public string NewsSubTitle;
+    public string NewsShortTitle;
+    public string NewsText;
+    public string NewsDate;
 
     public Announcement ToAnnouncement()
     {
         var result = new Announcement
         {
-            Number = Number,
-            Title = Title,
-            SubTitle = SubTitle,
-            ShortTitle = ShortTitle,
-            Text = Text,
+            NewsNumber = NewsNumber,
+            NewsTitle = NewsTitle,
+            NewsSubTitle = NewsSubTitle,
+            NewsShortTitle = NewsShortTitle,
+            NewsText = NewsText,
             Language = (uint)DataManager.Settings.Language.CurrentLanguage,
-            Date = Date,
+            NewsDate = NewsDate,
             Id = "ModNews"
         };
 
         return result;
     }
     public static List<ModNews> AllModNews = [];
-    public static string ModNewsURL = "https://raw.githubusercontent.com/0xDrMoe/TownofHost-Enhanced/main/Resources/Announcements/modNews-";
     static bool downloaded = false;
-    public ModNews(int Number, string Title, string SubTitle, string ShortTitle, string Text, string Date)
+    public ModNews(int NewsNumber, string NewsTitle, string NewsSubTitle, string NewsShortTitle, string NewsText, string NewsDate)
     {
-        this.Number = Number;
-        this.Title = Title;
-        this.SubTitle = SubTitle;
-        this.ShortTitle = ShortTitle;
-        this.Text = Text;
-        this.Date = Date;
+        this.NewsNumber = NewsNumber;
+        this.NewsTitle = NewsTitle;
+        this.NewsSubTitle = NewsSubTitle;
+        this.NewsShortTitle = NewsShortTitle;
+        this.NewsText = NewsText;
+        this.NewsDate = NewsDate;
         AllModNews.Add(this);
     }
 
@@ -66,19 +65,7 @@ public class ModNews
             downloaded = true;
             ModNewsURL += TranslationController.Instance.currentLanguage.languageID switch
             {
-                SupportedLangs.German => "de_DE.json",
-                SupportedLangs.Latam => "es_419.json",
-                SupportedLangs.Spanish => "es_ES.json",
-                SupportedLangs.Filipino => "fil_PH.json",
-                SupportedLangs.French => "fr_FR.json",
-                SupportedLangs.Italian => "it_IT.json",
-                SupportedLangs.Japanese => "ja_JP.json",
-                SupportedLangs.Korean => "ko_KR.json",
-                SupportedLangs.Dutch => "nl_NL.json",
-                SupportedLangs.Brazilian => "pt_BR.json",
-                SupportedLangs.Russian => "ru_RU.json",
                 SupportedLangs.SChinese => "zh_CN.json",
-                SupportedLangs.TChinese => "zh_TW.json",
                 _ => "en_US.json", //English and any other unsupported language
             };
             var request = UnityWebRequest.Get(ModNewsURL);
@@ -95,10 +82,10 @@ public class ModNews
 
             foreach (var newsElement in newsArray.EnumerateArray())
             {
-                var number = int.Parse(newsElement.GetProperty("Number").GetString());
-                var title = newsElement.GetProperty("Title").GetString();
-                var subTitle = newsElement.GetProperty("Subtitle").GetString();
-                var shortTitle = newsElement.GetProperty("Short").GetString();
+                var Newsnumber = int.Parse(newsElement.GetProperty("Number").GetString());
+                var Newstitle = newsElement.GetProperty("Title").GetString();
+                var NewssubTitle = newsElement.GetProperty("Subtitle").GetString();
+                var NewsshortTitle = newsElement.GetProperty("Short").GetString();
                 var body = newsElement.GetProperty("Body").EnumerateArray().ToStringEnumerable().ToString();
                 var dateString = newsElement.GetProperty("Date").GetString();
                 // Create ModNews object
@@ -142,34 +129,10 @@ public class ModNews
         obj.transform.localPosition = new Vector3(-0.8f, 0.13f, 0.5f);
         obj.transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
         var renderer = obj.AddComponent<SpriteRenderer>();
-        renderer.sprite = Utils.LoadSprite($"TOHE.Resources.Images.CreditsButton.png", 250f);
+        renderer.sprite = Utils.LoadSprite($"EHR-Icon.png", 250f);
         renderer.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
     }
 }
 
 
-//    [HarmonyPatch(typeof(PlayerAnnouncementData), nameof(PlayerAnnouncementData.SetAnnouncements)), HarmonyPrefix]
-//    public static bool SetModAnnouncements_Prefix(PlayerAnnouncementData __instance, [HarmonyArgument(0)] ref Il2CppReferenceArray<Announcement> aRange)
-//    {
-//        if (AllModNews.Count == 0)
-//        {
-//            Init();
-//            AllModNews.Sort((a1, a2) => { return DateTime.Compare(DateTime.Parse(a2.Date), DateTime.Parse(a1.Date)); });
-//        }
 
-//        List<Announcement> FinalAllNews = [];
-//        AllModNews.Do(n => FinalAllNews.Add(n.ToAnnouncement()));
-//        foreach (var news in aRange.ToArray())
-//        {
-//            if (!AllModNews.Any(x => x.Number == news.Number))
-//                FinalAllNews.Add(news);
-//        }
-//        FinalAllNews.Sort((a1, a2) => { return DateTime.Compare(DateTime.Parse(a2.Date), DateTime.Parse(a1.Date)); });
-
-//        aRange = new(FinalAllNews.Count);
-//        for (int i = 0; i < FinalAllNews.Count; i++)
-//            aRange[i] = FinalAllNews[i];
-
-//        return true;
-//    }
-//}
