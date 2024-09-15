81195e936f4eb6d56a92607a3d495b829f25e23b
ABoringCatto <1905810774@qq.com>
2024-09-15 11:56:27 +0800

    ModNews can work...maybe

--- Patches/AnnouncementPatch.cs
+++ Patches/AnnouncementPatch.cs
@@ -1,9 +1,11 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using AmongUs.Data;
+using LibCpp2IL;
 using AmongUs.Data.Player;
+using System.Text.Json;
 using Assets.InnerNet;
 using HarmonyLib;
 using Il2CppInterop.Runtime.InteropTypes.Arrays;
@@ -14,7 +16,9 @@ using UnityEngine.Networking;
 
 namespace EHR;
 
-public class ModNewsData
+// code credit https://github.com/Yumenopai/TownOfHost_Y
+[HarmonyPatch]
+public class ModNews
 {
     public int Number;
     public int BeforeNumber;
@@ -40,21 +44,10 @@ public class ModNewsData
 
         return result;
     }
-}
-
-[HarmonyPatch]
-public class ModNews
-{
-    public static List<ModNews> AllModNews = new();
-    public int Number;
-    public int BeforeNumber;
-    public string Title;
-    public string SubTitle;
-    public string ShortTitle;
-    public string Text;
-    public string Date;
-    public ModNews(int Number, string Title, string SubTitle, string ShortTitle,
-        string Text, string Date)
+    public static List<ModNews> AllModNews = [];
+    public static string ModNewsURL = "https://raw.githubusercontent.com/0xDrMoe/TownofHost-Enhanced/main/Resources/Announcements/modNews-";
+    static bool downloaded = false;
+    public ModNews(int Number, string Title, string SubTitle, string ShortTitle, string Text, string Date)
     {
         this.Number = Number;
         this.Title = Title;
@@ -65,27 +58,8 @@ public class ModNews
         AllModNews.Add(this);
     }
 
-    public Announcement ToAnnouncement()
-    {
-        var result = new Announcement
-        {
-            Number = Number,
-            Title = Title,
-            SubTitle = SubTitle,
-            ShortTitle = ShortTitle,
-            Text = Text,
-            Language = (uint)DataManager.Settings.Language.CurrentLanguage,
-            Date = Date,
-            Id = "ModNews"
-        };
-
-        return result;
-    }
-
-    public const string ModNewsURL = "https://raw.githubusercontent.com/AboringCat/EndlessHostRolesCN/main/modNews.json";
-    static bool downloaded = false;
     [HarmonyPatch(typeof(AnnouncementPopUp), nameof(AnnouncementPopUp.Init)), HarmonyPostfix]
-    public static void Initialize(ref Il2CppSystem.Collections.IEnumerator __result)
+    public static void Initialize_Postfix(ref Il2CppSystem.Collections.IEnumerator __result)
     {
         static IEnumerator FetchBlacklist()
         {
@@ -94,6 +68,23 @@ public class ModNews
                 yield break;
             }
             downloaded = true;
+            ModNewsURL += TranslationController.Instance.currentLanguage.languageID switch
+            {
+                SupportedLangs.German => "de_DE.json",
+                SupportedLangs.Latam => "es_419.json",
+                SupportedLangs.Spanish => "es_ES.json",
+                SupportedLangs.Filipino => "fil_PH.json",
+                SupportedLangs.French => "fr_FR.json",
+                SupportedLangs.Italian => "it_IT.json",
+                SupportedLangs.Japanese => "ja_JP.json",
+                SupportedLangs.Korean => "ko_KR.json",
+                SupportedLangs.Dutch => "nl_NL.json",
+                SupportedLangs.Brazilian => "pt_BR.json",
+                SupportedLangs.Russian => "ru_RU.json",
+                SupportedLangs.SChinese => "zh_CN.json",
+                SupportedLangs.TChinese => "zh_TW.json",
+                _ => "en_US.json", //English and any other unsupported language
+            };
             var request = UnityWebRequest.Get(ModNewsURL);
             yield return request.SendWebRequest();
             if (request.isNetworkError || request.isHttpError)
@@ -102,23 +93,33 @@ public class ModNews
                 Logger.Info("ModNews Error Fetch:" + request.responseCode.ToString(), "ModNews");
                 yield break;
             }
-            var json = JObject.Parse(request.downloadHandler.text);
-            for (var news = json["News"].First; news != null; news = news.Next)
+
+            var jsonDocument = JsonDocument.Parse(request.downloadHandler.text);
+            var newsArray = jsonDocument.RootElement.GetProperty("News");
+
+            foreach (var newsElement in newsArray.EnumerateArray())
             {
-                ModNews n = new(
-                    int.Parse(news["Number"].ToString()), news["Title"]?.ToString(), news["Subtitle"]?.ToString(), news["Short"]?.ToString(),
-                    news["Body"]?.ToString(), news["Date"]?.ToString());
+                var number = int.Parse(newsElement.GetProperty("Number").GetString());
+                var title = newsElement.GetProperty("Title").GetString();
+                var subTitle = newsElement.GetProperty("Subtitle").GetString();
+                var shortTitle = newsElement.GetProperty("Short").GetString();
+                var body = newsElement.GetProperty("Body").EnumerateArray().ToStringEnumerable().ToString();
+                var dateString = newsElement.GetProperty("Date").GetString();
+                // Create ModNews object
+                ModNews _ = new(number, title, subTitle, shortTitle, body, dateString);
             }
         }
         __result = Effects.Sequence(FetchBlacklist().WrapToIl2Cpp(), __result);
     }
+
+
     [HarmonyPatch(typeof(PlayerAnnouncementData), nameof(PlayerAnnouncementData.SetAnnouncements)), HarmonyPrefix]
-    public static bool SetModAnnouncements(PlayerAnnouncementData __instance, [HarmonyArgument(0)] ref Il2CppReferenceArray<Announcement> aRange)
+    public static bool SetModAnnouncements_Prefix(PlayerAnnouncementData __instance, [HarmonyArgument(0)] ref Il2CppReferenceArray<Announcement> aRange)
     {
         Logger.Info("AllModNews:" + AllModNews.Count, "ModNews");
         AllModNews.Sort((a1, a2) => { return DateTime.Compare(DateTime.Parse(a2.Date), DateTime.Parse(a1.Date)); });
 
-        List<Announcement> FinalAllNews = new();
+        List<Announcement> FinalAllNews = [];
         AllModNews.Do(n => FinalAllNews.Add(n.ToAnnouncement()));
         foreach (var news in aRange)
         {
@@ -134,17 +135,45 @@ public class ModNews
         return true;
     }
 
+
     [HarmonyPatch(typeof(AnnouncementPanel), nameof(AnnouncementPanel.SetUp)), HarmonyPostfix]
-    public static void SetUpPanel(AnnouncementPanel __instance, [HarmonyArgument(0)] Announcement announcement)
+    public static void SetUpPanel_Postfix(AnnouncementPanel __instance, [HarmonyArgument(0)] Announcement announcement)
     {
         if (announcement.Number < 100000) return;
         var obj = new GameObject("ModLabel");
-        obj.layer = -1;
+        //obj.layer = -1;
         obj.transform.SetParent(__instance.transform);
         obj.transform.localPosition = new Vector3(-0.8f, 0.13f, 0.5f);
         obj.transform.localScale = new Vector3(0.9f, 0.9f, 0.9f);
         var renderer = obj.AddComponent<SpriteRenderer>();
-        renderer.sprite = Utils.LoadSprite($"EHR.Resources.ModNews.png", 400f);
+        renderer.sprite = Utils.LoadSprite($"EHR.Resources.Images.ModNews.png", 250f);
         renderer.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
     }
 }
+
+
+//    [HarmonyPatch(typeof(PlayerAnnouncementData), nameof(PlayerAnnouncementData.SetAnnouncements)), HarmonyPrefix]
+//    public static bool SetModAnnouncements_Prefix(PlayerAnnouncementData __instance, [HarmonyArgument(0)] ref Il2CppReferenceArray<Announcement> aRange)
+//    {
+//        if (AllModNews.Count == 0)
+//        {
+//            Init();
+//            AllModNews.Sort((a1, a2) => { return DateTime.Compare(DateTime.Parse(a2.Date), DateTime.Parse(a1.Date)); });
+//        }
+
+//        List<Announcement> FinalAllNews = [];
+//        AllModNews.Do(n => FinalAllNews.Add(n.ToAnnouncement()));
+//        foreach (var news in aRange.ToArray())
+//        {
+//            if (!AllModNews.Any(x => x.Number == news.Number))
+//                FinalAllNews.Add(news);
+//        }
+//        FinalAllNews.Sort((a1, a2) => { return DateTime.Compare(DateTime.Parse(a2.Date), DateTime.Parse(a1.Date)); });
+
+//        aRange = new(FinalAllNews.Count);
+//        for (int i = 0; i < FinalAllNews.Count; i++)
+//            aRange[i] = FinalAllNews[i];
+
+//        return true;
+//    }
+//}
